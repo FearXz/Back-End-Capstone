e3ae08b5146a3ff2d728d988b672b656925a3bfa
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-04-04 21:14:04 +0200

    The most significant changes involve the addition of a new HTTP GET method `GetRistorante(int id)` to the `RistoranteController` class. This method retrieves specific restaurant data from the `Ristoranti` table in the database based on the provided `id` and returns it as an `IActionResult`. The returned data includes various properties of the restaurant and nested collections for `CategorieRistorante`, `GiorniDiChiusura`, and `Prodotti`.
    List of Changes:
    
    1. A new HTTP GET method `GetRistorante(int id)` has been added to the `RistoranteController` class. This method takes an integer `id` as a parameter and returns an `IActionResult`. The method queries the `Ristoranti` table in the database, filtering for a restaurant with the provided `id` and where `IsAttivo` is `true`.
    
    2. The `GetRistorante(int id)` method selects and returns a new anonymous object containing various properties of the restaurant, including `IdAzienda`, `PartitaIva`, `IdRistorante`, `NomeRistorante`, `Indirizzo`, `Citta`, `CAP`, `Latitudine`, `Longitudine`, `Telefono`, `OrarioApertura`, `OrarioChiusura`, `ImgCopertina`, `ImgLogo`, `Descrizione`, `TagRistorante`.
    
    3. The method also includes nested collections for `CategorieRistorante`, `GiorniDiChiusura`, and `Prodotti`, each of which is a selection of related data from other tables in the database.
    
    4. The `Prodotti` collection includes further nested collections for `Ingredienti` and `TipiProdotto`.
    
    5. The method ends by returning the selected restaurant data wrapped in an `Ok` response.

--- Back-End-Capstone/Controllers/RistoranteController.cs
+++ Back-End-Capstone/Controllers/RistoranteController.cs
@@ -59,5 +59,68 @@ namespace Back_End_Capstone.Controllers
 
             return Ok(categorie);
         }
+
+        [HttpGet("localeId/{id}")]
+        public IActionResult GetRistorante(int id)
+        {
+            var ristorante = _db
+                .Ristoranti.Where(r => r.IdRistorante == id && r.IsAttivo == true)
+                .Select(ristorante => new
+                {
+                    ristorante.IdAzienda,
+                    ristorante.Azienda.PartitaIva,
+                    ristorante.IdRistorante,
+                    ristorante.NomeRistorante,
+                    ristorante.Indirizzo,
+                    ristorante.Citta,
+                    ristorante.CAP,
+                    ristorante.Latitudine,
+                    ristorante.Longitudine,
+                    ristorante.Telefono,
+                    ristorante.OrarioApertura,
+                    ristorante.OrarioChiusura,
+                    ristorante.ImgCopertina,
+                    ristorante.ImgLogo,
+                    ristorante.Descrizione,
+                    ristorante.TagRistorante,
+                    CategorieRistorante = ristorante.CategorieRistorante.Select(cr => new
+                    {
+                        cr.IdCategorie,
+                        cr.Categorie.NomeCategoria
+                    }),
+                    GiorniDiChiusura = ristorante.GiorniChiusuraRistorante.Select(g => new
+                    {
+                        g.GiorniChiusura.NomeGiorno,
+                        g.GiorniChiusura.NumeroGiorno
+                    }),
+                    Prodotti = ristorante
+                        .ProdottiRistorante.Where(state => state.IsAttivo == true)
+                        .Select(pr => new
+                        {
+                            pr.IdProdottoRistorante,
+                            pr.NomeProdotto,
+                            pr.PrezzoProdotto,
+                            pr.DescrizioneProdotto,
+                            pr.ImgProdotto,
+                            Ingredienti = pr
+                                .IngredientiProdottoRistorante.Where(state =>
+                                    state.IngredientiRistorante.IsAttivo
+                                )
+                                .Select(ip => new
+                                {
+                                    ip.IngredientiRistorante.IdIngrediente,
+                                    ip.IngredientiRistorante.NomeIngrediente,
+                                    ip.IngredientiRistorante.PrezzoIngrediente,
+                                }),
+                            TipiProdotto = pr.ProdottoTipoProdotti.Select(ptp => new
+                            {
+                                ptp.TipoProdotto.IdTipoProdotto,
+                                ptp.TipoProdotto.NomeTipoProdotto,
+                            }),
+                        }),
+                });
+
+            return Ok(ristorante);
+        }
     }
 }
